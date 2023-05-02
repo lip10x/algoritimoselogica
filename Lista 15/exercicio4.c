4d505cd38606f3b186de3cd2f182b444d09c9984
lip10x <lipgms29@gmail.com>
2023-05-02 19:35:58 -0300

    exercicio 4

---
+++ Lista 15/exercicio4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main(){
+	int num, num2, soma;
+	printf("Informe um numero: ");
+	scanf("%d", &num);
+	
+	printf("Informe outro numero: ");
+	scanf("%d", &num2);
+	soma = num + num2;
+	
+	printf("Soma: %d", soma);
+	
+	
+}
