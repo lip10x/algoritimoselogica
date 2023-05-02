607fd08901d8a0d3c1133ffc07e89970d224b36e
lip10x <lipgms29@gmail.com>
2023-05-02 20:08:48 -0300

    exercicio 6

---
+++ Lista 15/exercicio6.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main(){
+	
+	int num , num2 , num3;
+	int div;
+	
+	printf("Informe um numero: ");
+	scanf("%d", &num);
+	printf("Informe um numero: ");
+	scanf("%d", &num2);
+	printf("Informe um numero: ");
+	scanf("%d", &num3);
+	
+	div = (num - num2) / 2;
+	
+	printf("Total: %d", div);
+	
+	
+	
+}
